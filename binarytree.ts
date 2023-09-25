d3b0b7e6af232f169406ae8323940627b27a59e9
endulum <71853916+endulum@users.noreply.github.com>
2023-09-25 15:34:14 -0700

    add and remove individual nodes

--- binarytree.ts
+++ binarytree.ts
@@ -31,6 +31,67 @@ class Tree {
         // by the end, the subtrees will conjoin into a single-rooted tree
         return newNode;
     }
+    
+    add (value: number, node = this.root) {
+        if (!node) return;
+        // is the value greater than current node?
+        if (value > node.value) {
+            // does the current node have a right node to see? if so, recurse
+            if (node.rightNode) this.add(value, node.rightNode);
+            // if not, perform the addition
+            else node.rightNode = new Subtree(value);
+        } 
+        
+        // same thing in the other direction
+        else if (value < node.value) {
+            if (node.leftNode) this.add(value, node.leftNode);
+            else node.leftNode = new Subtree(value);
+        }
+    }
+
+    remove (value: number, node = this.root): Subtree | null {
+        if (!node) return null;
+
+        // use recursion to find the node
+        if (value > node.value) {
+            node.rightNode = this.remove(value, node.rightNode);
+            return node;
+        } else if (value < node.value) {
+            node.leftNode = this.remove(value, node.leftNode);
+            return node;
+        }
+
+        // if we've reached the node we want to remove,
+        // neither of the conditions above should be met.
+        // we proceed:
+
+        if (!node.leftNode) {
+            return node.rightNode;
+        } else if (!node.rightNode) {
+            return node.leftNode;
+        } 
+
+        // if the node we want to remove
+        // has no or one child, our work is easy.
+        // but if there are two children, we have some
+        // more work to do...
+        
+        else {
+            let parent = node;
+            let successor = node.rightNode;
+            while (successor.leftNode) {
+                parent = successor;
+                successor = successor.leftNode;
+            }
+            if (parent !== node) {
+                parent.leftNode = successor.rightNode;
+            } else {
+                parent.rightNode = successor.rightNode;
+            }
+            node.value = successor.value;
+            return node;
+        }
+    }
 
     prettyPrint (node = this.root, prefix = "", isLeft = true) {
         if (!node) return;
@@ -45,10 +106,12 @@ class Tree {
     }
 }
 
+const MAX = 20;
+
 // generate an array of random values
-const data = new Array(15) // empties
+const data = new Array(10) // empties
                  .fill(1)  // set all to "1"
-                 .map(v => v * Math.ceil(Math.random() * 100)) // multiply each by random number
+                 .map(v => v * Math.ceil(Math.random() * MAX)) // multiply each by random number
                  .sort((a, b) => a - b) // sort ascending
                  .filter(function(value, index, array) {
                     return !index || value != array[index - 1]; // filter out dupes
@@ -57,4 +120,22 @@ const data = new Array(15) // empties
 console.log(data);
 
 const myTree = new Tree(data);
+
+// add() testing:
+// generate random nonexisting number to add
+let newNumber = Math.ceil(Math.random() * MAX);
+while (data.includes(newNumber)) newNumber = Math.ceil(Math.random() * MAX);
+myTree.prettyPrint();
+console.log(`Adding new number "${newNumber}"...`);
+myTree.add(newNumber);
+myTree.prettyPrint();
+
+console.log();
+
+// remove() testing:
+// generate random existing number to remove
+let deleteNumber = data[Math.floor(Math.random() * data.length)];
+myTree.prettyPrint();
+console.log(`Deleting number "${deleteNumber}"...`);
+myTree.remove(deleteNumber);
 myTree.prettyPrint();
