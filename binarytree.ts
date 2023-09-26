62d8e1a4a27b45121af5dfe07e976d8186737716
endulum <71853916+endulum@users.noreply.github.com>
2023-09-25 19:18:26 -0700

    tidying

--- binarytree.ts
+++ binarytree.ts
@@ -1,7 +1,7 @@
-class Subtree {
+class TreeNode {
     value: number;
-    leftNode: Subtree | null = null;
-    rightNode: Subtree | null = null;
+    leftNode: TreeNode | null = null;
+    rightNode: TreeNode | null = null;
 
     constructor (value: number) {
         this.value = value;
@@ -9,51 +9,46 @@ class Subtree {
 }
 
 class Tree {
-    root: Subtree | null;
+    root: TreeNode | null;
 
     constructor (data: number[]) {
         this.root = this.build(data, 0, data.length - 1);
     }
 
     build (array: number[], leftIndex: number, rightIndex: number) {
-        // stop condition: if the leftmost index surpasses the right
-        if (leftIndex > rightIndex) return null;
-
-        // get the center of the array
-        const middleIndex: number = Math.ceil((leftIndex + rightIndex) / 2);
-        // make it the "root" of this subtree
-        const newNode = new Subtree(array[middleIndex]);
-
-        // make new subtrees by recursing into each half of the array
-        newNode.leftNode = this.build(array, leftIndex, middleIndex - 1);
-        newNode.rightNode = this.build(array, middleIndex + 1, rightIndex);
-
-        // by the end, the subtrees will conjoin into a single-rooted tree
-        return newNode;
+        if (leftIndex > rightIndex) {
+            return null;
+        } else {
+            const middleIndex: number = Math.ceil((leftIndex + rightIndex) / 2);
+            const newNode = new TreeNode(array[middleIndex]);
+            newNode.leftNode = this.build(array, leftIndex, middleIndex - 1);
+            newNode.rightNode = this.build(array, middleIndex + 1, rightIndex);
+            return newNode;
+        }
     }
     
     add (value: number, node = this.root) {
-        if (!node) return;
-        // is the value greater than current node?
-        if (value > node.value) {
-            // does the current node have a right node to see? if so, recurse
-            if (node.rightNode) this.add(value, node.rightNode);
-            // if not, perform the addition
-            else node.rightNode = new Subtree(value);
-        } 
-        
-        // same thing in the other direction
-        else if (value < node.value) {
-            if (node.leftNode) this.add(value, node.leftNode);
-            else node.leftNode = new Subtree(value);
+        if (!node) {
+            return;
+        } else if (value > node.value) {
+            if (node.rightNode) {
+                this.add(value, node.rightNode);
+            } else {
+                node.rightNode = new TreeNode(value);
+            }
+        } else if (value < node.value) {
+            if (node.leftNode) {
+                this.add(value, node.leftNode);
+            } else {
+                node.leftNode = new TreeNode(value);
+            }
         }
     }
 
-    remove (value: number, node = this.root): Subtree | null {
-        if (!node) return null;
-
-        // use recursion to find the node
-        if (value > node.value) {
+    remove (value: number, node = this.root): TreeNode | null {
+        if (!node) {
+            return null;
+        } else if (value > node.value) {
             node.rightNode = this.remove(value, node.rightNode);
             return node;
         } else if (value < node.value) {
@@ -61,95 +56,69 @@ class Tree {
             return node;
         }
 
-        // if we've reached the node we want to remove,
-        // neither of the conditions above should be met.
-        // we proceed:
-
         if (!node.leftNode) {
             return node.rightNode;
         } else if (!node.rightNode) {
             return node.leftNode;
-        } 
-
-        // if the node we want to remove
-        // has no or one child, our work is easy.
-        // but if there are two children, we have some
-        // more work to do...
+        }
         
         else {
             let parent = node;
             let successor = node.rightNode;
+
             while (successor.leftNode) {
                 parent = successor;
                 successor = successor.leftNode;
             }
+            
             if (parent !== node) {
                 parent.leftNode = successor.rightNode;
             } else {
                 parent.rightNode = successor.rightNode;
             }
+
             node.value = successor.value;
             return node;
         }
     }
 
-    find (value: number, node = this.root): Subtree | null {
-        if (!node) return null;
-        else if (value > node.value) return this.find(value, node.rightNode);
-        else if (value < node.value) return this.find(value, node.leftNode);
-        return node;
+    find (value: number, node = this.root): TreeNode | null {
+        if (!node) {
+            return null;
+        } else if (value > node.value) {
+            return this.find(value, node.rightNode);
+        } else if (value < node.value) {
+            return this.find(value, node.leftNode);
+        } else {
+            return node;
+        }
     }
 
     prettyPrint (node = this.root, prefix = "", isLeft = true) {
         if (!node) return;
 
-        if (node.rightNode)
+        if (node.rightNode) {
             this.prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
 
         console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
 
-        if (node.leftNode)
+        if (node.leftNode) {
             this.prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
     }
 }
 
 const MAX = 20;
 
-// generate an array of random values
-const data = new Array(10) // empties
-                 .fill(1)  // set all to "1"
-                 .map(v => v * Math.ceil(Math.random() * MAX)) // multiply each by random number
-                 .sort((a, b) => a - b) // sort ascending
+const data = new Array(10).fill(1)
+                 .map(v => v * Math.ceil(Math.random() * MAX))
+                 .sort((a, b) => a - b)
                  .filter(function(value, index, array) {
-                    return !index || value != array[index - 1]; // filter out dupes
+                    return !index || value != array[index - 1];
                  });
 
 console.log(data);
 
 const myTree = new Tree(data);
-
-// // add() testing:
-// // generate random nonexisting number to add
-// let newNumber = Math.ceil(Math.random() * MAX);
-// while (data.includes(newNumber)) newNumber = Math.ceil(Math.random() * MAX);
-// myTree.prettyPrint();
-// console.log(`Adding new number "${newNumber}"...`);
-// myTree.add(newNumber);
-// myTree.prettyPrint();
-
-// console.log();
-
-// // remove() testing:
-// // generate random existing number to remove
-// let deleteNumber = data[Math.floor(Math.random() * data.length)];
-// myTree.prettyPrint();
-// console.log(`Deleting number "${deleteNumber}"...`);
-// myTree.remove(deleteNumber);
-// myTree.prettyPrint();
-
-// find() testing:
-// generate random existing number to find
-let findNumber = data[Math.floor(Math.random() * data.length)];
 myTree.prettyPrint();
-console.log(`Finding number "${findNumber}"...`);
-console.log(myTree.find(findNumber));
